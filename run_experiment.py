11f9495769afc2e74dba2c6a5f1f5d5a844f4fb1
paynesa <paynesa@sas.upenn.edu>
2020-10-18 01:36:04 -0400

    run 1000 times

--- run_experiment.py
+++ run_experiment.py
@@ -1,8 +1,25 @@
 from curricula import get_curriculum, get_verification
 from proposebutverify import PbvLearner
-curriculum = get_curriculum("curricula/train.txt")
-verification = get_verification("curricula/train.gold")
+train_curriculum = get_curriculum("curricula/train.txt")
+train_verification = get_verification("curricula/train.gold")
+test_curriculum = get_curriculum("curricula/test.txt")
+test_verification = get_curriculum("curricula/test.gold")
 learner = PbvLearner()
-learner.observe(curriculum)
-precision, recall, f_score = learner.evaluate(verification)
+precision = 0
+recall = 0
+f_score = 0
+for i in range(1000):
+    learner.observe(train_curriculum)
+    p, r, f = learner.evaluate(train_verification)
+    precision += p
+    recall += r
+    f_score += f
+precision = precision / 1000
+recall = recall / 1000
+f_score = f_score / 1000
 print(precision, recall, f_score)
+
+
+
+
+
