752b7a4d0182ac7a0f56727377624e82e7018d8a
paynesa <paynesa@sas.upenn.edu>
2020-10-23 14:47:12 -0400

    param optimization continued

--- optimize_pursuit.py
+++ optimize_pursuit.py
@@ -1,4 +1,16 @@
-from pursuit.optimize import optimize_pursuit
+from pursuit.optimize import optimize_pursuit, run_pursuit_experiment
 
-best_gamma, best_lamda, best_threshold = optimize_pursuit(True, 1)
-print(best_gamma, best_lamda, best_threshold)
+print("Getting optimized paramters over 100 instances for pursuit without sampling...")
+best_gamma, best_lamda, best_threshold = optimize_pursuit(False, 100)
+print(f"\nBest parameters: {best_gamma}, {best_lamda}, {best_threshold}")
+
+print(
+    f"Running pursuit experiments with gamma={best_gamma}, lamda={best_lamda}, threshold={best_threshold}"
+)
+run_pursuit_experiment(
+    gamma_learning_rate=best_gamma,
+    lambda_smothing=best_lamda,
+    threshold=best_threshold,
+    num_iterations=1000,
+    pursuit_sampling=False,
+)
